fb2462dbdabc5199b77dfdc61576f7bb087de1c3
benjamin mayeur <benjaminmayeur@hotmail.fr>
2024-11-21 16:09:39 +0100

    resolving error log about verify-email for production-bis

--- app/api/verify-email/route.ts
+++ app/api/verify-email/route.ts
@@ -1,28 +1,28 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+// import { prisma } from "@/lib/prisma";
+// import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-    // const { searchParams } = new URL(req.url);
-    // const token = searchParams.get('token');
-    // const roomId = searchParams.get('roomId');
-    // const hotelId = searchParams.get('hotelId');
+// export async function GET(req: Request) {
+//     const { searchParams } = new URL(req.url);
+//     const token = searchParams.get('token');
+//     const roomId = searchParams.get('roomId');
+//     const hotelId = searchParams.get('hotelId');
   
-    // const verificationToken = await prisma.verificationToken.findUnique({
-    //   where: { token },
-    // });
+//     const verificationToken = await prisma.verificationToken.findUnique({
+//       where: { token },
+//     });
   
-    // if (!verificationToken || verificationToken.expires < new Date()) {
-    //   return NextResponse.redirect('/token-expired');
-    // }
+//     if (!verificationToken || verificationToken.expires < new Date()) {
+//       return NextResponse.redirect('/token-expired');
+//     }
   
-    // await prisma.user.update({
-    //   where: { email },
-    //   data: { emailVerified: new Date() },
-    // });
+//     await prisma.user.update({
+//       where: { email },
+//       data: { emailVerified: new Date() },
+//     });
   
-    // await prisma.verificationToken.delete({
-    //   where: { token },
-    // });
+//     await prisma.verificationToken.delete({
+//       where: { token },
+//     });
   
-    // return NextResponse.redirect(`/hotels/${hotelId}/rooms/${roomId}/booking`);
-  }
+//     return NextResponse.redirect(`/hotels/${hotelId}/rooms/${roomId}/booking`);
+//   }
